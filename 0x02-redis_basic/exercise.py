292f20f4a26cc0fc0969e755cf3363ec87de8cee
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-23 23:07:59 +0300

    Refactor Redis caching functionality and test cases

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""doc doc module"""
-
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -8,80 +6,75 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """doc doc class"""
-
+    ''' def count calls '''
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """doc doc class"""
-        key = method.__qualname__
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper '''
+        key_name = method.__qualname__
+        self._redis.incr(key_name, 0) + 1
+        return method(self, *args, **kwds)
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """doc doc class"""
-    inkey = method.__qualname__ + ":inputs"
-    outkey = method.__qualname__ + ":outputs"
-
+    ''' def call history '''
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """doc doc class"""
-        self._redis.rpush(inkey, str(args))
-        res = method(self, *args, **kwargs)
-        self._redis.rpush(outkey, str(res))
-        return res
-
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper'''
+        key_m = method.__qualname__
+        inp_m = key_m + ':inputs'
+        outp_m = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp_m, data)
+        fin = method(self, *args, **kwds)
+        self._redis.rpush(outp_m, str(fin))
+        return fin
     return wrapper
 
 
-def replay(method: Callable) -> None:
-    """doc doc class"""
-    input_key = "{}:inputs".format(method.__qualname__)
-    output_key = "{}:outputs".format(method.__qualname__)
-
-    inputs = method.__self__._redis.lrange(input_key, 0, -1)
-    outputs = method.__self__._redis.lrange(output_key, 0, -1)
-
-    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
-    for inp, out in zip(inputs, outputs):
-        print(
-            "{}(*{}) -> {}".format(
-                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
-            )
+def replay(func: Callable):
+    '''def replay'''
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m,
+            k.decode('utf-8'),
+            v.decode('utf-8')
         )
+        print(fin)
 
 
-class Cache:
-    """doc doc class"""
-
+class Cache():
+    ''' class cache '''
     def __init__(self):
-        """doc doc method"""
+        ''' def init '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
-    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """doc doc method"""
-        keyx = str(uuid.uuid4())
-        self._redis.set(keyx, data)
-        return keyx
+        ''' def store '''
+        generate = str(uuid.uuid4())
+        self._redis.set(generate, data)
+        return generate
 
-    def get(
-        self, key: str, fn: Optional[Callable] = None
-    ) -> Union[str, bytes, int, float]:
-        """doc doc method"""
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' def get '''
         value = self._redis.get(key)
-        if fn:
-            value = fn(value)
-        return value
+        return value if not fn else fn(value)
 
-    def get_str(self, key: str) -> str:
-        """doc doc method"""
-        return self.get(key, fn=str)
+    def get_int(self, key):
+        return self.get(key, int)
 
-    def get_int(self, key: str) -> int:
-        """doc doc method"""
-        return self.get(key, fn=int)
+    def get_str(self, key):
+        value = self._redis.get(key)
+        return value.decode("utf-8")
